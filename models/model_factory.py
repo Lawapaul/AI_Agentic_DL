2d2e0556e09b8eacc583f8d29dfcf67a721a271b
Lawapaul <harshitshekhawat501@gmail.com>
2026-02-15 21:02:10 +0530

    Fixed cnn import name in model_factory

--- models/model_factory.py
+++ models/model_factory.py
@@ -1,10 +1,10 @@
-from models.cnn_model import build_cnn_model
+from models.cnn_model import build_ids_cnn_model
 from models.hybrid_cnn_lstm import build_hybrid_model
 
 def get_model(model_type, input_shape, num_classes):
 
     if model_type == "cnn":
-        return build_cnn_model(input_shape, num_classes)
+        return build_ids_cnn_model(input_shape, num_classes)
 
     elif model_type == "hybrid":
         return build_hybrid_model(input_shape, num_classes)
